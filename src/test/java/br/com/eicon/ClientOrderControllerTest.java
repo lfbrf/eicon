814c69f07c7af78549b5cbe27a525fd45515bf60
Luiz Felipe <luizfelipebasile@gmail.com>
2020-11-02 02:17:56 -0300

    Refatorando index e ajustando classe de teste

--- src/test/java/br/com/eicon/ClientOrderControllerTest.java
+++ src/test/java/br/com/eicon/ClientOrderControllerTest.java
@@ -11,12 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.eicon.service.ClientOrderService;
 
